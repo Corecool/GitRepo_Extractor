2dc6cbb3370c81b5130733c03d5d9f4379101a36
Corecool <wowwowxyr@126.com>
2013-07-06 11:51:42 +0800

    能够显示两次代码提交间受影响的模块

--- convert.rb
+++ convert.rb
@@ -1,25 +1,24 @@
 #!/usr/bin/env ruby
 
 require 'csv'
-# require 'pry'
+require 'pry'
 
 if ARGV.size != 2
-	puts 'usage: convert.rb input output'
+	puts 'usage: convert.rb projectRoot outputFile.csv'
 	exit
 end
 
-if File.exist?(ARGF.filename)
-	$input = IO.read(ARGF.filename)
-else
-	puts "Input File is not exist."
-	exit
-end
-$input.gsub!("\t", "    ")
+$projectRoot = ARGF.filename
 ARGF.skip
+
+Dir.chdir(File.expand_path($projectRoot)) {|path|
+	$input = `git log`
+}
+$input.gsub!("\t", "    ")
 
 system("touch #{ARGV[0]}") unless File.exist?(ARGV[0])
 $file = CSV.open(ARGF.filename, "wb")
-$file << ['CommitID', 'MergeIDs', 'Author', 'Email', 'CommitDate', 'Content']
+$file << ['CommitID', 'MergeIDs', 'Author', 'Email', 'CommitDate', 'ChangeModules', 'Content']
 
 $row = []
 
@@ -31,6 +30,17 @@ def handle_content(ppos, pos)
 	$row << $input[ppos...pos]
 end
 
+def handle_influenceModules(curCommit, lastCommit)
+	mods, data = "", ""
+	Dir.chdir(File.expand_path($projectRoot)) {|path|
+		data = `git diff #{curCommit[:Commit]} #{lastCommit[:Commit]} --stat`
+	}
+	data.each_line{|line|
+		mods << line.sub(/([^|]*)\|.*/, '\1') 
+	}	
+	$row << mods
+end
+
 $ppos, $pos = 0, 0
 $pattern = /commit\s*(?<Commit>.*)\n(Merge:\s(?<Merge>.{7}\s.{7})\n)?Author:\s*(?<Author>[^<]*)<(?<Email>.*)>\nDate:\s*(?<Date>.*)\n/
 while data = $pattern.match($input, $pos)
@@ -39,8 +49,10 @@ while data = $pattern.match($input, $pos)
 	$ppos = $pos = data.end(0)
 	if matchData = $pattern.match($input, $pos)
 		$pos = matchData.begin(0)
+		handle_influenceModules(data, matchData)
 		handle_content($ppos, $pos)
 	else
+		$row << ""
 		handle_content($ppos, $input.size)
 	end
 	$file << $row
